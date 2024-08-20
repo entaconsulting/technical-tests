681db928887a386b6fb4c78baadb4ac5c50c848f
Facundo H. Litterio <facundolitterio7@gmail.com>
2024-08-20 08:49:23 -0300

    Agregado de prueba tecnica React Dev

---
+++ prueba-tecnica-react-developer/src/App.tsx
@@ -0,0 +1,10 @@
+export const API_URL: string =
+  "https://api.freeapi.app/api/v1/public/randomusers";
+
+const App = () => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>{/* Code here */}</div>
+  );
+};
+
+export default App;
